79765ee7005cfe0616bde9ce9eb5792d18ac3527
nacen.dev <nacen.dev@gmail.com>
2022-03-21 17:11:04 +0800

    Create checkGameWinner function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const BOARD_SIZE = 9;
 let currentPlayer = 1;
 
+const winnerDisplay = document.querySelector(".winner-display");
+const gameBoardElement = document.querySelector(".gameboard");
+
+const Player = (name) => {
+  return {
+    name,
+  };
+};
+
 const gameBoard = (() => {
   const board = new Array(BOARD_SIZE);
 
@@ -10,18 +19,66 @@ const gameBoard = (() => {
     board[index] = input;
   };
 
+  const horizontalCheck = (input) => {
+    for (let i = 0; i < 9; i += 3) {
+      if (
+        board[i] === input &&
+        board[i + 1] === input &&
+        board[i + 2] === input
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const verticalCheck = (input) => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i] === input &&
+        board[i + 3] === input &&
+        board[i + 6] === input
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const diagonalCheck = (input) => {
+    if (
+      (board[0] === input || board[2] === input) &&
+      board[4] === input &&
+      (board[8] === input || board[6] === input)
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const checkGameWinner = (input) => {
+    return horizontalCheck(input)
+      ? true
+      : verticalCheck(input)
+      ? true
+      : diagonalCheck(input)
+      ? true
+      : false;
+  };
+
   return {
     getBoard,
     setBoardItem,
+    checkGameWinner,
   };
 })();
 
 const displayController = ((gameBoard) => {
-  const { getBoard, setBoardItem } = gameBoard;
+  const { getBoard, setBoardItem, checkGameWinner } = gameBoard;
+  
   const board = getBoard();
 
   const renderBoard = () => {
-    const gameBoardElement = document.querySelector(".gameboard");
     for (let i = 0; i < BOARD_SIZE; i++) {
       gameBoardElement.appendChild(createGameBoardCell(i));
     }
@@ -33,9 +90,18 @@ const displayController = ((gameBoard) => {
     gameBoardCell.addEventListener("click", (event) => {
       const value = currentPlayer === 1 ? "X" : "O";
 
+      if (winnerDisplay.textContent) {
+        return;
+      }
+
       if (!board[index]) {
         setBoardItem(index, value);
         event.target.textContent = value;
+        if (checkGameWinner(value)) {
+          winnerDisplay.textContent = "WIN";
+          console.log(gameBoardElement);
+        }
+
         currentPlayer = currentPlayer === 1 ? 2 : 1;
       }
     });
@@ -54,23 +120,5 @@ const displayController = ((gameBoard) => {
     renderBoard,
   };
 })(gameBoard);
-
-const Player = (name) => {
-  let score = 0;
-  const incrementScore = () => {
-    score += 1;
-  };
-
-  const getScore = () => score;
-
-  return {
-    name,
-    getScore,
-    incrementScore,
-  };
-};
-
-let player1 = Player("Player 1");
-let player2 = Player("Player 2");
 
 displayController.renderBoard([]);
