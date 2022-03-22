ffa80aa2e2d0458051b8075da7491087fb30ca0c
nacen.dev <nacen.dev@gmail.com>
2022-03-22 18:46:56 +0800

    Add setPlayerTurn and setActivePlayerDisplay

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ let currentPlayer = 1;
 
 const winnerDisplay = document.querySelector(".winner-display");
 const gameBoardElement = document.querySelector(".gameboard");
+const menu = document.querySelector(".menu");
+const gameArea = document.querySelector(".game-area");
+const gameStartButton = document.querySelector(".menu-start");
+const playerOneText = document.querySelector(".player-one");
+const playerTwoText = document.querySelector(".player-two");
 
 const Player = (name) => {
+  let score = 0;
+
+  const getScore = () => {
+    return score;
+  };
+
+  const incrementScore = () => {
+    score += 1;
+    return score;
+  };
+
+  turn = false;
+
   return {
     name,
+    getScore,
+    incrementScore,
+    turn,
   };
 };
+
+const PlayerOne = Player("Player 1");
+const PlayerTwo = Player("Player 2");
 
 const gameBoard = (() => {
   const board = new Array(BOARD_SIZE);
@@ -46,11 +70,9 @@ const gameBoard = (() => {
   };
 
   const diagonalCheck = (input) => {
-    if (
-      (board[0] === input || board[2] === input) &&
-      board[4] === input &&
-      (board[8] === input || board[6] === input)
-    ) {
+    if (board[0] === input && board[4] === input && board[8] === input) {
+      return true;
+    } else if (board[2] === input && board[4] === input && board[6] === input) {
       return true;
     }
     return false;
@@ -73,9 +95,9 @@ const gameBoard = (() => {
   };
 })();
 
-const displayController = ((gameBoard) => {
+const displayController = ((gameBoard, menu, playerOne, playerTwo) => {
   const { getBoard, setBoardItem, checkGameWinner } = gameBoard;
-  
+
   const board = getBoard();
 
   const renderBoard = () => {
@@ -84,11 +106,31 @@ const displayController = ((gameBoard) => {
     }
   };
 
+  const setActivePlayerDisplay = (playerOne, playerTwo) => {
+    if (playerOne.turn) {
+      playerTwoText.classList.remove("current-player");
+      playerOneText.classList.add("current-player");
+    } else if (playerTwo.turn) {
+      playerOneText.classList.remove("current-player");
+      playerTwoText.classList.add("current-player");
+    }
+  };
+
+  const setPlayerTurn = (playerOne, playerTwo) => {
+    if (playerOne.turn) {
+      playerOne.turn = false;
+      playerTwo.turn = true;
+    } else {
+      playerOne.turn = true;
+      playerTwo.turn = false;
+    }
+  };
+
   const markBoardCellEvent = (gameBoardCell) => {
     const index = gameBoardCell.dataset.index;
 
     gameBoardCell.addEventListener("click", (event) => {
-      const value = currentPlayer === 1 ? "X" : "O";
+      const value = PlayerOne.turn ? "X" : "O";
 
       if (winnerDisplay.textContent) {
         return;
@@ -97,12 +139,16 @@ const displayController = ((gameBoard) => {
       if (!board[index]) {
         setBoardItem(index, value);
         event.target.textContent = value;
+
         if (checkGameWinner(value)) {
-          winnerDisplay.textContent = "WIN";
-          console.log(gameBoardElement);
+          winnerDisplay.textContent = PlayerOne.turn
+            ? `${PlayerOne.name} WIN!`
+            : `${PlayerTwo.name} WIN!`;
+          return;
         }
 
-        currentPlayer = currentPlayer === 1 ? 2 : 1;
+        setPlayerTurn(PlayerOne, PlayerTwo);
+        setActivePlayerDisplay(PlayerOne, PlayerTwo);
       }
     });
   };
@@ -116,9 +162,40 @@ const displayController = ((gameBoard) => {
     return gameBoardCell;
   };
 
+  const getPlayerNamesFromDisplay = () => {
+    const playerNames = document.querySelectorAll(".player-name-input");
+    let [playerOneName, playerTwoName] = playerNames;
+    PlayerOne.name = playerOneName.value ? playerOneName.value : PlayerOne.name;
+    PlayerTwo.name = playerTwoName.value ? playerTwoName.value : PlayerTwo.name;
+  };
+
+  const setPlayerNameInDisplay = () => {
+    playerOne.textContent = PlayerOne.name;
+    playerTwo.textContent = PlayerTwo.name;
+  };
+
+  const gameStart = () => {
+    console.log("start game");
+    menu.className = "hidden";
+    gameArea.classList.remove("hidden");
+  };
+
   return {
     renderBoard,
+    gameStart,
+    getPlayerNamesFromDisplay,
+    setPlayerNameInDisplay,
+    setPlayerTurn,
+    setActivePlayerDisplay,
   };
-})(gameBoard);
+})(gameBoard, menu, playerOneText, playerTwoText);
 
 displayController.renderBoard([]);
+
+gameStartButton.addEventListener("click", () => {
+  displayController.gameStart();
+  displayController.getPlayerNamesFromDisplay();
+  displayController.setPlayerNameInDisplay();
+  displayController.setPlayerTurn(PlayerOne, PlayerTwo);
+  displayController.setActivePlayerDisplay(PlayerOne, PlayerTwo);
+});
